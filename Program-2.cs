b249246ca2b8b261e6bcce5fac09c4c07044b7fc
Juliolopez20 <118144500+Juliolopez20@users.noreply.github.com>
2022-11-16 19:48:58 -0600

    Add files via upload

---
+++ Program-2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Hojadetrabajo7_1030021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoja de Tranajo no.7");
+            
+            double t;
+            double Celsius;
+
+            Console.WriteLine("Ingrese la temperatura registrada");
+            Solicitartemp();
+                double resultado = Calculotemp(t);
+
+                Console.WriteLine("La temperatura en grados celsius es: " + resultado.ToString ());
+
+
+                void Solicitartemp()
+                {
+                    t = Convert.ToDouble(Console.ReadLine());
+                }
+
+                double Calculotemp(double t)
+                {
+
+                    Celsius = (t - 32) * (5 / 9);
+                    return Celsius;
+                }
+            
+
+
+        }
+    }
+}
---
+++ Program-3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hoja_de_trabajo_no_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area de un circulo");
+
+            double r, pi;
+            double area;
+
+            r = 0;
+            pi = 3.14;
+           
+            Console.WriteLine("Ingresar el valor del radio con decimales");
+            SolicitarDatos();
+            double resultado = calculoArea(r);
+            Console.WriteLine("El valor del area es: " + resultado.ToString());
+
+            void SolicitarDatos()
+            {
+                r = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+
+            double calculoArea(double radio)
+            {
+
+                area = pi * Math.Pow(r, 2);
+                return area;
+            }
+
+        }
+    }
+}
